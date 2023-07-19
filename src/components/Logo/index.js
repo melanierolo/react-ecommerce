7f40ca5c0745fbc5f13ebe9f0cd5b61ff015f4f4
Melanie Rodas <mrodasl@uni.pe>
2023-07-18 20:08:04 -0500

    add a NavLink '/'

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import logo from "../../assets/icons/logo.png";
 import "./style.css";
+import { NavLink } from "react-router-dom";
 
 export default function Logo() {
+  console.log("Vamos a home");
   return (
-    <div className="logo">
-      <img className="logo__logotipo" src={logo} alt="logo" />
-      <p className="logo__name">DigitalStore</p>
-    </div>
+    <>
+      <NavLink to="/">
+        <div className="logo">
+          <img className="logo__logotipo" src={logo} alt="logo" />
+          <p className="logo__name">DigitalStore</p>
+        </div>
+      </NavLink>
+    </>
   );
 }
