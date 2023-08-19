a6dd076c3423699de7b52da53b19ea50669bab43
Melanie Rodas <mrodasl@uni.pe>
2023-08-18 23:51:32 -0500

    add numberOfProducts in CardWidget with useContext(CartContext)

--- src/components/CardWidget/index.js
+++ src/components/CardWidget/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import iconShopping from '../../assets/icons/shopping-cart.png';
 import { Link } from 'react-router-dom';
 import './style.css';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
 
 export default function CardWidget() {
+  const { numberOfProducts } = useContext(CartContext);
   return (
     <div className='shopping'>
       <Link to='/cart'>
@@ -13,6 +16,7 @@ export default function CardWidget() {
           src={iconShopping}
           alt='icon shopping cart'
         />
+        <strong>{numberOfProducts}</strong>
       </Link>
     </div>
   );
