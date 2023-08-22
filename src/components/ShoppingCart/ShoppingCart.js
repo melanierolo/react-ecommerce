d14f726fb2f7413e0d955dc59a7caed1d68efc5b
Melanie Rodas <mrodasl@uni.pe>
2023-08-21 20:18:59 -0500

    added css to the shoppingCart and shoppingCartItem

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
 import { Link } from 'react-router-dom';
 import ShoppingItem from '../ShoppingItem/ShoppingItem';
+import './style.css';
 
 function ShoppingCart() {
   const { cart, clearCart, total, numberOfProducts } = useContext(CartContext);
@@ -15,27 +16,31 @@ function ShoppingCart() {
     );
   }
   return (
-    <>
-      <div>
-        <div>
-          <h2>Productos</h2>
+    <section className='cart'>
+      <div className='cart'>
+        <div className='cart-products'>
+          <h2 className='cart-products__title'>Productos</h2>
           {cart.map((product) => (
             <ShoppingItem key={product.item.id} {...product} />
           ))}
+          <button className='cart-clear-button' onClick={() => clearCart()}>
+            Eliminar Todo
+          </button>
         </div>
-        <button onClick={() => clearCart()}>Eliminar Todo</button>
       </div>
-      <div>
-        <h2>Detalles de compra:</h2>
-        <p>Productos: {numberOfProducts}</p>
-        <p>Subtotal :${total}</p>
-        <p>Envío : $9 </p>
-        <p>Total :${total + 9}</p>
-        <Link to='/checkout'>
-          <button>Finalizar compra</button>
+      <div className='cart-summary'>
+        <h2 className='cart-summary__title'>Detalles de compra:</h2>
+        <p className='cart-summary__info'>Productos: {numberOfProducts}</p>
+        <p className='cart-summary__info'>Subtotal: ${total}</p>
+        <p className='cart-summary__info'>Envío: $9</p>
+        <p className='cart-summary__info'>Total: ${total + 9}</p>
+        <Link to='/checkout' className='cart-summary__checkout-link'>
+          <button className='cart-summary__checkout-button'>
+            Finalizar compra
+          </button>
         </Link>
       </div>
-    </>
+    </section>
   );
 }
 
--- src/components/ShoppingItem/ShoppingItem.js
+++ src/components/ShoppingItem/ShoppingItem.js
@@ -1,15 +1,23 @@
 import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
+import './style.css';
 
 function ShoppingItem({ item, quantityOfProducts }) {
   const { removeItemFromCart } = useContext(CartContext);
   return (
-    <div>
-      <h4>{item.name}</h4>
-      <p>Cantidad:{quantityOfProducts}</p>
-      <p>Precio:{item.price}</p>
-      <button onClick={() => removeItemFromCart(item.id)}>Eliminar</button>
-      <hr></hr>
+    <div className='shopping-cart-item'>
+      <h4 className='shopping-cart-item__name'>{item.name}</h4>
+      <p className='shopping-cart-item__quantity'>
+        Cantidad: {quantityOfProducts}
+      </p>
+      <p className='shopping-cart-item__price'>Precio: {item.price}</p>
+      <button
+        className='shopping-cart-item__button'
+        onClick={() => removeItemFromCart(item.id)}
+      >
+        Eliminar
+      </button>
+      <hr />
     </div>
   );
 }
